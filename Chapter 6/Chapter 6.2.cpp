b06457a1b81c378005eeb1da6e76e5ed8c0649ff
taejin-seong <sungtj0130@naver.com>
2022-01-06 00:38:49 +0900

    -

---
+++ Chapter 6/Chapter 6.2.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <cstring>
+
+//#define START1
+#define START2			
+		
+
+using namespace std;
+
+
+
+
+
+#ifdef START1
+
+//MyFriendClass.cpp
+class Girl;
+
+class Boy
+{
+	private:
+		int height;
+		friend class Girl;
+		
+	public:
+		Boy(int len) : height(len){}
+		
+		void ShowYourFriendInfo(Girl &frn);
+};
+
+
+class Girl
+{
+	private:
+		char phNum[20];
+	
+	public:
+		Girl(char* num)
+		{
+			strcpy(phNum, num);
+		}
+		
+		void ShowYourFriendInfo(Boy &frn);
+		
+		friend class Boy;
+};
+
+
+void Boy::ShowYourFriendInfo(Girl &frn)
+{
+	cout<<"Her phone number: "<<frn.phNum<<endl;
+
+}
+
+void Girl::ShowYourFriendInfo(Boy &frn)
+{
+	cout<<"His height: "<<frn.height<<endl;
+}
+
+int main(void)
+{
+	Boy boy(170);
+	Girl girl("010-1234-5678");
+	
+	boy.ShowYourFriendInfo(girl);
+	girl.ShowYourFriendInfo(boy);
+	
+	return 0;
+}
+
+#endif /* START1 */
+
+
+
+
+#ifdef START2
+
+//MyFriendFunction.cpp
+class Point;
+
+class PointOP
+{
+	private:
+		int opcnt;
+	
+	public:
+		PointOP() :opcnt(0) {}
+		
+		Point PointAdd(const Point&, const Point&);
+		
+		Point PointSub(const Point&, const Point&);
+		
+		~PointOP()
+		{
+			cout<<"Operation times: "<<opcnt<<endl;
+		}
+		
+};
+
+
+class Point
+{
+	private:
+		int x;
+		int y;
+	
+	public:
+		Point(const int &xpos, const int &ypos) : x(xpos), y(ypos) {}
+		
+		friend Point PointOP::PointAdd(const Point&, const Point&);
+		
+		friend Point PointOP::PointSub(const Point&, const Point&);
+		
+		friend void ShowPointPos(const Point&);	
+
+};
+
+
+Point PointOP::PointAdd(const Point& pnt1, const Point& pnt2)
+{
+	opcnt++;
+	return Point(pnt1.x + pnt2.x, pnt1.y + pnt2.y);
+}
+
+Point PointOP::PointSub(const Point& pnt1, const Point& pnt2)
+{
+	opcnt++;
+	return Point(pnt1.x - pnt2.x, pnt1.y - pnt2.y);
+}
+
+int main(void)
+{
+	Point pos1(1, 2);
+	Point pos2(2, 4);
+	PointOP op;
+	
+	ShowPointPos(op.PointAdd(pos1, pos2));
+	ShowPointPos(op.PointSub(pos2, pos1));
+	
+	return 0;
+}
+
+
+void ShowPointPos(const Point& pos)
+{
+	cout<<"x: "<<pos.x<<", ";
+	cout<<"y: "<<pos.y<<endl;
+}
+#endif /* START2 */
+
---
+++ Chapter 6/Chapter 6.3.cpp
@@ -0,0 +1,244 @@
+#include <iostream>
+
+//#define START1
+//#define START2			
+//#define START3
+//#define START4
+//#define START5
+#define START6
+
+using namespace std;
+
+
+
+
+
+#ifdef START1
+
+//CComStatic.cpp
+void Counter()
+{
+	static int cnt;
+	cnt ++;
+	
+	cout<<"Current cnt: "<<cnt<<endl;
+}
+
+int main(void)
+{
+	for(int i=0; i<10; i++)
+	{
+		Counter();
+	}
+	
+	return 0;
+}
+#endif /* START1 */
+
+
+
+
+#ifdef START2
+
+//NeedGlobal.cpp
+
+int simObjCnt = 0;
+int cmxObjCnt = 0;
+
+class SoSimple
+{
+	public:
+		SoSimple()
+		{
+			simObjCnt++;
+			cout<<simObjCnt<<"¹øÂ° SoSimple °´Ã¼"<<endl;
+		}
+};
+
+class SoComplex
+{
+	public:
+		SoComplex()
+		{
+			cmxObjCnt++;
+			cout<<cmxObjCnt<<"¹øÂ° SoComplex °´Ã¼"<<endl;
+		}
+		
+		SoComplex(SoComplex &copy)
+		{
+			cmxObjCnt ++;
+			cout<<cmxObjCnt<<"¹øÂ° SoComplex °´Ã¼"<<endl;
+		}
+};
+
+int main(void)
+{
+	SoSimple  sim1;
+	SoSimple  sim2;
+	
+	SoComplex com1;
+	SoComplex com2 = com1;
+	
+	SoComplex();
+	
+	return 0;
+}
+#endif /* START2 */
+
+
+
+
+
+#ifdef START3
+
+//StaticMember.cpp
+class SoSimple
+{
+	private:
+		static int simObjCnt;
+	
+	public:
+		SoSimple()
+		{
+			simObjCnt ++;
+			cout<<simObjCnt<<"¹ø¤Š SoSimple °´Ã¼"<<endl;
+		}
+};
+
+int SoSimple::simObjCnt = 0;
+
+class SoComplex
+{
+	private:
+		static int cmxObjCnt;
+		
+	public:
+		SoComplex()
+		{
+			cmxObjCnt ++;
+			cout<<cmxObjCnt<<"¹øÂ° SoComplex °´Ã¼"<<endl;
+		}
+		
+		SoComplex(SoComplex &copy)
+		{
+			cmxObjCnt ++;
+			cout<<cmxObjCnt<<"¹øÂ° SoComplex °´Ã¼"<<endl;
+		}
+};
+
+int SoComplex::cmxObjCnt = 0;
+
+int main(void)
+{
+	SoSimple  sim1;
+	SoSimple  sim2;
+	
+	SoComplex com1;
+	SoComplex com2 = com1;
+	
+	SoComplex();
+	
+	return 0;
+}
+#endif /* START3 */
+
+
+
+
+
+#ifdef START4 
+
+//PublicStaticMember.cpp
+class SoSimple
+{
+	public:
+		static int simObjCnt;
+	
+	public:
+		SoSimple()
+		{
+			simObjCnt++;
+		}
+};
+
+int SoSimple::simObjCnt = 0;
+
+
+int main(void)
+{
+	cout<<SoSimple::simObjCnt<<"¹ø¤Š SoSimple °´Ã¼"<<endl;
+	SoSimple sim1;
+	SoSimple sim2;
+	
+	cout<<SoSimple::simObjCnt<<"¹øÂ° SoSimple °´Ã¼"<<endl;
+	cout<<sim1.simObjCnt<<"¹øÂ° SoSimple °´Ã¼"<<endl;
+	cout<<sim2.simObjCnt<<"¹øÂ° SoSimple °´Ã¼"<<endl;
+	 
+	return 0;
+} 
+#endif /* START4 */
+
+
+
+
+#ifdef START5
+
+//ConstStaticMember.cpp
+class CountryArea
+{
+	public:
+		const static int RUSSIA       =1707540;
+		const static int CANADA       =998467;
+		const static int CHINA        =957290;
+		const static int SOUTH_KOREA  =9922;
+		
+};
+
+int main(void)
+{
+	cout<<"·¯½Ã¾Æ ¸éÀû: "<<CountryArea::RUSSIA<<"§´"<<endl;
+	cout<<"Ä³³ª´Ù ¸éÀû: "<<CountryArea::CANADA<<"§´"<<endl;
+	cout<<"  Áß±¹ ¸éÀû: "<<CountryArea::CHINA<<"§´"<<endl;
+	cout<<"  ÇÑ±¹ ¸éÀû: "<<CountryArea::SOUTH_KOREA<<"§´"<<endl; 
+	
+	return 0;
+}
+#endif /* START5 */
+
+
+
+
+#ifdef START6
+
+//Mutable.cpp
+class SoSimple
+{
+	private:
+		int num1;
+		mutable int num2;
+		
+	public:
+		SoSimple(int n1, int n2) : num1(n1), num2(n2){}
+		
+		void ShowSimpleData() const
+		{
+			cout<<num1<<", "<<num2<<endl;
+		}
+		
+		void CopyToNum2() const
+		{
+			num2 = num1;
+		}
+};
+
+int main(void)
+{
+	SoSimple sm(1, 2);
+	sm.ShowSimpleData();
+	sm.CopyToNum2();	
+	sm.ShowSimpleData();
+	
+	return 0;
+}
+#endif /* START6 */
+
