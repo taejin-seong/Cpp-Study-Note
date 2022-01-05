16d85365cc60a91c61edb0e50a000a22f89f8c5d
taejin-seong <sungtj0130@naver.com>
2022-01-06 00:36:48 +0900

    -

---
+++ Chapter 6/Chapter 6.1.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+//#define START1
+#define START2			
+
+using namespace std;
+
+
+
+
+
+#ifdef START1
+
+//ConstObject.cpp
+class SoSimple
+{
+	private:
+		int num;
+	
+	public:
+		SoSimple(int n) : num(n) {}
+		
+		SoSimple& AddNum(int n)
+		{
+			num += n;
+			return *this;
+		}
+		
+		void ShowData() const
+		{
+			cout<<"num: "<<num<<endl;
+		}
+	
+};
+
+int main(void)
+{
+	const SoSimple obj(7);
+	//obj.AddNum(20);
+	
+	obj.ShowData();
+	
+	return 0;
+}
+#endif /* START1 */
+
+
+
+
+#ifdef START2
+
+//ConstOverloading.cpp
+class SoSimple
+{
+	private:
+		int num;
+	
+	public:
+		SoSimple(int n) :num(n){
+		}
+		
+		SoSimple& AddNum(int n)
+		{
+			num += n;
+			
+			return *this;
+		}
+		
+		void SimpleFunc()
+		{
+			cout<<"SimpleFunc: "<<num<<endl;
+		}
+		
+		void SimpleFunc() const
+		{
+			cout<<"const SimpleFunc: "<<num<<endl; 
+		}
+};
+
+void YourFunc(const SoSimple &obj)
+{
+	obj.SimpleFunc();
+}
+
+int main(void)
+{
+	SoSimple obj1(2);
+	const SoSimple obj2(7);
+	
+	obj1.SimpleFunc();
+	obj2.SimpleFunc();
+	
+	YourFunc(obj1);
+	YourFunc(obj2);
+}
+#endif /* START2 */
+
+
