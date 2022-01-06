2bacc152822c816d56c4461badcf64ae14a1395b
taejin-seong <sungtj0130@naver.com>
2022-01-06 17:08:30 +0900

    Before starting Inheritance

---
+++ Chapter 7/Chapter 7.1.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class PermanentWorker //정규직 직원의 관리 목적 클래스 (데이터 성격) 
+{
+	private:
+		char name[100];
+		int salary;
+		
+	public:
+		PermanentWorker(char* name, int money) : salary(money)
+		{
+			
+			strcpy(this->name, name);
+		
+		}
+		
+		int GetPay() const
+		{
+			return salary;
+		}
+		
+		void ShowSalaryInfo() const
+		{
+			cout<<"name: "<<name<<endl;
+			cout<<"salary: "<<GetPay()<<endl<<endl;
+		}
+};
+
+class EmployeeHandler // PermanentWorker의 객체를 저장 및 관리하기 위한 클래스 (기능적 성격) -> ** 핸들러 클래스 **
+{
+	private:
+		PermanentWorker* empList[50];
+		int empNum;
+		
+	public:
+		 EmployeeHandler() : empNum(0) {};
+		 
+		 void AddEmployee(PermanentWorker* emp)
+		 {
+		 	empList[empNum ++] = emp;
+		 }
+		 
+		 void ShowAllSalaryInfo() const
+		 {
+		 	for (int i = 0; i < empNum; i++)
+		 	{
+		 		empList[i]->ShowSalaryInfo();
+			 }
+		 }
+		 
+		 void ShowTotalSalary() const
+		 {
+		 	int sum = 0;
+		 	for (int i = 0; i < empNum; i++)
+		 	{
+		 		sum += empList[i]->GetPay();
+			}
+			
+			cout<<"salary sum:  "<<sum<<endl;
+		 }
+		 
+		 ~EmployeeHandler()
+		 {
+		 	for(int i = 0; i < empNum; i++)
+		 	{
+		 		delete empList[i];
+			}
+		 }		 
+		 
+};
+
+int main(void)
+{
+	//직원관리를 목적으로 설계된 컨트롤 클래스의 객체생성 
+	EmployeeHandler handler;
+	
+	//직원 등록 
+	handler.AddEmployee(new PermanentWorker("KIM", 1000));
+	handler.AddEmployee(new PermanentWorker("LEE", 1500));
+	handler.AddEmployee(new PermanentWorker("JUN", 2000));
+	
+	//이번 달에 지불해야 할 급여의 정보
+	handler.ShowAllSalaryInfo();
+	
+	//이번 달에 지불해야 할 급여의 총합
+	handler.ShowTotalSalary();
+	
+	return 0;
+	
+}
