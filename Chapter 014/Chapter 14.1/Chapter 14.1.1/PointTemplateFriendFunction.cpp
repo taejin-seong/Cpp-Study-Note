9db3e0504514742600635aa877a1bcffa9d0faed
taejin-seong <sungtj0130@naver.com>
2022-01-14 20:18:01 +0900

    Extensions of the content studied in Chapter 13

---
+++ Chapter 014/Chapter 14.1/Chapter 14.1.1/PointTemplateFriendFunction.cpp
@@ -0,0 +1,11 @@
+#include "Point.h"
+
+
+Point::Point(int x,  int y) : xpos(x), ypos(y) { }
+
+ostream& operator<<(ostream& os, const Point& pos)
+{
+	os<<'['<<pos.xpos<<", "<<pos.ypos<<']'<<endl;
+	
+	return os;
+}
