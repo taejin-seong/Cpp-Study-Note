77ff9df0536787694cf9904ab52a65c86ac76844
taejin-seong <sungtj0130@naver.com>
2022-01-15 20:00:23 +0900

    Understand the situation of exceptions and handling exceptions

---
+++ Chapter 015/Chapter 15.1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+
+//#define START1
+#define START2
+
+
+
+using namespace std;
+
+
+#ifdef START1
+
+//UnhandlingException.cpp
+int main(void)
+{
+	int num1, num2;
+	cout<<"두 개의 숫자 입력: ";
+	cin>>num1>>num2;
+	
+	cout<<"나눗셈의 몫: "<<num1/num2<<endl;
+	cout<<"나눗셈의 나머지: "<<num1%num2<<endl;
+	
+	return 0;
+}
+#endif /* START1 */
+
+
+
+
+
+#ifdef START2
+
+//HandlingExceptionUseIf.cpp
+int main(void)
+{
+	
+	int num1, num2;
+	cout<<"두 개의 숫자 입력: ";
+	cin>>num1>>num2;
+	
+	if (num2 == 0)
+	{
+		cout<<"제수는 0이 될 수 없습니다."<<endl;
+		cout<<"프로그램을 다시 실행하세요."<<endl;
+	}
+	
+	else
+	{
+		cout<<"나눗셈의 몫: "<<num1/num2<<endl;
+		cout<<"나눗셈의 나머지: "<<num1%num2<<endl;	
+	}
+	
+	return 0;
+}
+#endif /* START2 */
