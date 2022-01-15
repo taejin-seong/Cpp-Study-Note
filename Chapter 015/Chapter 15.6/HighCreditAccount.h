9fc76943737f26f1d720c8c8cd42f3b8665cbc1c
taejin-seong <sungtj0130@naver.com>
2022-01-15 20:09:38 +0900

    OOP 11단계

---
+++ Chapter 015/Chapter 15.6/HighCreditAccount.h
@@ -0,0 +1,40 @@
+/*
+ * HighCreditAccount.h
+ *
+ *  Created on: Jan 6, 2022
+ *      Author: Tae Jin
+ */
+
+
+#ifndef HIGH_CREDIT_ACCOUNT_H
+#define HIGH_CREDIT_ACCOUNT_H 
+
+
+#include "NormalAccount.h"
+
+
+//½Å¿ë½Å·Ú°èÁÂ 
+class HighCreditAccount : public NormalAccount  
+{
+	private:
+		int PlusRate;
+
+	public:
+		HighCreditAccount(int id, int money, String name, int rate, int plus) : NormalAccount(id, money, name, rate), PlusRate(plus) 
+		{ } 
+		
+		
+		virtual void Deposit(int money)
+		{
+			if (money < 0)
+			{
+				throw MinusException(money);
+			} 
+			
+			NormalAccount::Deposit(money);
+			Account::Deposit(money * (PlusRate / 100.0));
+		}			
+	
+};
+
+#endif /* HIGH_CREDIT_ACCOUNT_H */
---
+++ Chapter 015/Chapter 15.6/NormalAccount.h
@@ -0,0 +1,39 @@
+/*
+ * NormalAccount.h
+ *
+ *  Created on: Jan 6, 2022
+ *      Author: Tae Jin
+ */
+
+
+#ifndef NORMAL_ACCOUNT_H 
+#define NORMAL_ACCOUNT_H 
+
+
+#include "Account.h"
+
+ 
+//보통예금게좌 
+class NormalAccount : public Account  
+{
+	private:
+		int InterestRate;
+	
+	public:
+		NormalAccount(int id, int money, String name, int rate) : Account(id, money, name), InterestRate(rate) 
+		{ } 
+	
+		
+		virtual void Deposit(int money)
+		{
+			if (money < 0)
+			{
+				throw MinusException(money);
+			}
+			
+			Account::Deposit(money);
+			Account::Deposit(money* (InterestRate / 100.0));
+		}		
+};
+
+#endif /* NORMAL_ACCOUNT_H */
